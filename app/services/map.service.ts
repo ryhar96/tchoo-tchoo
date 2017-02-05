abe59616b492dfe2b940ee5394a22f8747998554
Ryan <hardiemusic@gmail.com>
2017-02-04 15:37:24 -1000

    RH ddhff

--- app/services/map.service.ts
+++ app/services/map.service.ts
@@ -1,12 +1,14 @@
 import { Injectable, OnInit } from '@angular/core';
 import { AgmCoreModule } from 'angular2-google-maps/core';
 
-declare var google:any;
+declare var goog:any;
 
 @Injectable()
 export class MapService {
     
     public map: any;
+
+    public google: any = goog;
 
     init(map: any) {
         this.map = map;
