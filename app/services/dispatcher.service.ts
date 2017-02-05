dddac2c7a85b9c0b20852c3541d53d233517cc9d
Timothee CHAUVIN <timothee.chauvin@polymtl.ca>
2017-02-05 02:07:05 -0500

    TC : quand gaspard cru avoir raison (il s'en ai enfiler une)

--- app/services/dispatcher.service.ts
+++ app/services/dispatcher.service.ts
@@ -9,7 +9,7 @@ import { MapService } from './map.service';
 import { Observable } from 'rxjs/Rx';
 
 const LAT_MAX = 45.548669;
-const LAT_MIN = 45.548286;
+const LAT_MIN = 45.450963;
 
 const LON_MIN = -73.754601;
 const LON_MAX = -73.532482;
